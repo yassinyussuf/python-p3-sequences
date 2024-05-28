ddb2dc2d86fb778c364cf95a8e8c0ec45a7327a8
yassinyussuf <yassinyussuf414@gmail.com>
2024-05-28 09:30:31 +0300

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print ([]) 
+        return
+
+    fibonacci_sequence = [0, 1]
+    
+    while len(fibonacci_sequence) < length:
+        next_value = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+        fibonacci_sequence.append(next_value)
+    
+    fibonacci_sequence = fibonacci_sequence[:length]
+
+    print(fibonacci_sequence)
